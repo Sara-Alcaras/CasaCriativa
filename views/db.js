99afdccffa0da72b4d2e4c9d30f9952bbedc6ef0
Sara-Alcaras <saraalcaras401@gmail.com>
2020-06-09 11:27:26 -0300

    creating table

--- views/db.js
+++ views/db.js
@@ -4,7 +4,16 @@ const db = new sqlite3.Database('./ws.db')
 db.serialize(function () {
 
     //criar a tabela
-    db.run("CREATE TABLE IF NOT EXISTS ideas")
+    db.run(`
+    CREATE TABLE IF NOT EXISTS ideas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        image TEXT,
+        title TEXT,
+        category TEXT,
+        description TEXT,
+        link TEXT
+        );
+    `)
 
     //inserir dado na tabela
 
