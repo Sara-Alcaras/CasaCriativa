91fe13aade8c06489da64a887501461e5cbde65e
Sara-Alcaras <saraalcaras401@gmail.com>
2020-06-09 12:11:03 -0300

    delete data from the table and query data

--- db.js
+++ db.js
@@ -16,29 +16,45 @@ db.serialize(function () {
     `)
 
     //inserir dado na tabela
-    db.run(`
-        INSERT INTO ideas (
-            image,
-            title,
-            category ,
-            description
-            link
-        ) VALUES (?,?,?,?,?);    
-    `
-    const values = [
-        "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-        "Cursos de Programação",
-        "Estudo",
-        "Dar o próximo passo na sua evolução como programador, potencialize o seu aprendizado e destaque-se entre os programadores.",
-        "https://www.rocketseat.com.br"
+   // const query = `
+    //INSERT INTO ideas (
+      //  image,
+      //  title,
+       // category ,
+       // description,
+       // link
+    //) VALUES (?,?,?,?,?);    
+    //`
+   
+    //const values = [
+       // "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        //"Cursos de Programação",
+       // "Estudo",
+        //"Dar o próximo passo na sua evolução como programador, potencialize o seu aprendizado e destaque-se entre os programadores.",
+        //"https://www.rocketseat.com.br"
 
-    ]
+   // ]
 
-    db.run(query, values, function (err) {
-        if (err) return console.log(err)
+    //db.run(query, values, function (err) {
+       //if (err) return console.log(err)
 
-        console.log(this)
-    })
+      // console.log(this)
+   // })
+
+    //deletar um dado da tabela
+    //db.run(`DELETE FROM ideas WHERE ID = ?`, [1], function(err) {
+       // if (err) return console.log(err)
+
+        //console.log("DELETEI", this)
+   // })
 
     //consultar dados na tabela
+    //db.all(`SELECT * FROM ideas`, function(err, rows) {
+       // if (err) return console.log(err)
+
+        //console.log(rows)
+   // })
+    
 })
+
+module.exports = db
